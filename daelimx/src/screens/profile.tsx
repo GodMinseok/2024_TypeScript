fec6a8b65cbb15c85a2e41494f9db913cbea71f2
GodMinseok <mseok830@naver.com>
2024-09-11 20:56:25 +0900

    여러 Page 추가

--- daelimx/src/screens/profile.tsx
+++ daelimx/src/screens/profile.tsx
@@ -5,7 +5,7 @@ import styled from "styled-components"
 const Container = styled.div``;
 const Title = styled.h1``;
 export default ()=>{
-    return <Container>
+    return <Container> 
         <Title>Profile Page.</Title>
     </Container>
 }
