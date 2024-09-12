19b5e90654a87161eb932e3b71ed3b7b99a725fe
GodMinseok <mseok830@naver.com>
2024-09-12 15:15:01 +0900

    Input 칸 위치 조절

--- daelimx/src/screens/signup.tsx
+++ daelimx/src/screens/signup.tsx
@@ -2,13 +2,33 @@
 
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column; // 세로 배치
+  align-items: center;
+  width: 80%;
+  max-width: 500px;
+  padding: 30px;
+`;
+
 const Title = styled.h1``;
 // 로고 이미지
 // Text 입력 필드 구역
-const Form = styled.form``;
+const Form = styled.form`
+  margin-top: 30px;
+  gap: 10px; // 칸 마다 간격
+  display: flex;
+  flex-direction: column;
+`;
+
 // Text 입력칸
-const Input = styled.input``;
+const Input = styled.input`
+  border-radius: 5px;
+  border: none;
+  padding: 5px 20px;
+`;
+
+const SubTitle = styled.p``;
 
 export default () => {
   return (
