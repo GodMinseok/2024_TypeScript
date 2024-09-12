e276bb2bab98d00982310d3d894e7d7c999534ac
GodMinseok <mseok830@naver.com>
2024-09-12 14:43:42 +0900

    ${reset} style 추가

--- daelimx/src/App.tsx
+++ daelimx/src/App.tsx
@@ -1,49 +1,67 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import Home from "./screens/home";
 import Profile from "./screens/profile";
 import Signin from "./screens/signin";
 import Signup from "./screens/signup";
+import reset from "styled-reset";
 
 // React-Router-Dom 을 활용해 사이트의 page 관리
 // - Page : home, progile, signin, signup
-const router = createBrowserRouter([{
-  path:"/",
-  children:[{
-    // home
-    path:"",
-    element: <Home/>
-  },{
-    // profile
-    path:"profile",
-    element: <Profile/>
-  }]
-},{
-  // signin
-  path:"/signin",
-  element : <Signin/>
-},{
-  // signup
-  path:"/signup",
-  element : <Signup/>
-}])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        // home
+        path: "",
+        element: <Home />,
+      },
+      {
+        // profile
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    // signin
+    path: "/signin",
+    element: <Signin />,
+  },
+  {
+    // signup
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
 
-
+// 중앙정렬 & 화면크기만큼
 const Container = styled.div`
-    background-color: forestgreen;
-    height: 300px;
-    width: 300px;
-`;// backtick
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`; // backtick
 
 const Title = styled.span``;
 
 function App() {
   return (
     <Container className="App">
-      <RouterProvider router={router} ></RouterProvider>
+      <GlobalStyle />
+      <RouterProvider router={router}></RouterProvider>
     </Container>
   );
 }
 
-
 export default App;
+
+// 공통적으로 전역에서 사용할 Global CSS Style
+const GlobalStyle = createGlobalStyle`
+${reset}
+body{
+  background-color: black;
+  color: white;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+}
+`;
