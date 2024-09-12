2d27881263e472c7189cd9a9cb75cacb4750ca6e
GodMinseok <mseok830@naver.com>
2024-09-12 14:44:18 +0900

    메세지 위치 조정 및 Input칸 만들기

--- daelimx/src/screens/signup.tsx
+++ daelimx/src/screens/signup.tsx
@@ -1,11 +1,25 @@
 // Signup page를 구성
 
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Container = styled.div``;
 const Title = styled.h1``;
-export default ()=>{
-    return <Container> 
-        <Title>Signup Page.</Title>
+// 로고 이미지
+// Text 입력 필드 구역
+const Form = styled.form``;
+// Text 입력칸
+const Input = styled.input``;
+
+export default () => {
+  return (
+    <Container>
+      <Title>Signup Page.</Title>
+      <Form>
+        <Input />
+        <Input />
+        <Input />
+        <Input />
+      </Form>
     </Container>
-}
+  );
+};
