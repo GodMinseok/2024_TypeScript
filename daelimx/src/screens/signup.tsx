4b0b8786bd559002a66188f1e3c13c4d1c001f43
GodMinseok <mseok830@naver.com>
2024-09-12 16:33:17 +0900

    회원 가입을 위한 Process 작성

--- daelimx/src/screens/signup.tsx
+++ daelimx/src/screens/signup.tsx
@@ -1,5 +1,6 @@
 // Signup page를 구성
 
+import { useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -15,6 +16,7 @@ const Title = styled.h1`
   font-size: 25px;
   font-weight: bold;
 `;
+
 // 로고 이미지
 // Text 입력 필드 구역
 const Form = styled.form`
@@ -43,17 +45,66 @@ const SubTitle = styled.p`
 `;
 
 export default () => {
+  // 회원 가입을 위한 Process 작성
+
+  // A. 입력한 회원 정보를 저장(State) -- useState Hook
+  const [nickName, setNickName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  // B. 입력한 회원 정보를 가공/수정한다.
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // 2. 입력한 정보(입력 값, 입력 위치)
+    // const name = event.target.name; // 입력 위치
+    // const value = event.target.value; // 입력 값
+    const {
+      target: { name, value },
+    } = event;
+    // 1. 입력한 정보를 분류(닉네임, 이메일, 비번)
+    switch (name) {
+      case "nickname":
+        console.log("Name:", value);
+        break;
+      case "email":
+        console.log("Email:", value);
+        break;
+      case "password":
+        console.log("PW:", value);
+        break;
+    }
+    // Goal: 각각 정보를 State(닉네임, 이메일, 비번) 저장
+  };
+
+  // C. 가입버튼을 누른 경우, 입력한 회원 정보를 SERVER에 전달 > 회원가입 처리
+  const onSubmit = () => {};
+
+  // Page Design Rndering (화면 디자인)
   return (
     <Container>
       <Title>회원 가입하기</Title>
       <Form>
         <SubTitle>이름*</SubTitle>
-        <Input type="text" placeholder="예) Minseok" />
+        <Input
+          name="nickname"
+          onChange={onChange}
+          type="text"
+          placeholder="예) Minseok"
+        />
         <SubTitle>이메일*</SubTitle>
-        <Input type="email" placeholder="에) Minseok@naver.com" />
+        <Input
+          name="email"
+          onChange={onChange}
+          type="email"
+          placeholder="에) Minseok@naver.com"
+        />
         <SubTitle>비밀번호*</SubTitle>
-        <Input type="password" placeholder="예) 6자리 이상 입력하세요." />
-        <Input type="submit" value={"가입하기"} />
+        <Input
+          name="password"
+          onChange={onChange}
+          type="password"
+          placeholder="예) 6자리 이상 입력하세요."
+        />
+        <Input name="submit" type="submit" value={"가입하기"} />
       </Form>
     </Container>
   );
