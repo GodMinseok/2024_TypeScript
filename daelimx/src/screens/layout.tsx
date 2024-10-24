b8f48d1e75126be7bfafdb9de63a5f0e531c9d84
GodMinseok <mseok830@naver.com>
2024-10-24 16:49:06 +0900

    로그아웃 버튼 추가

--- daelimx/src/screens/layout.tsx
+++ daelimx/src/screens/layout.tsx
@@ -92,13 +92,15 @@ export default () => {
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
-              fill="currentColor"
+              fill="none"
               viewBox="0 0 24 24"
             >
               <path
-                fill-rule="evenodd"
-                d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
-                clip-rule="evenodd"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2"
               />
             </svg>
           </MenuItem>
