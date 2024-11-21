f2b89e541d9472ed975d46335a3cc7897468bcae
GodMinseok <mseok830@naver.com>
2024-11-21 16:10:19 +0900

    게시판 댓글 실시간으로 업데이트 기능 추가

--- daelimx/src/components/Timeline.tsx
+++ daelimx/src/components/Timeline.tsx
@@ -2,7 +2,14 @@
 
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+  Unsubscribe,
+} from "firebase/firestore";
 import { firestore } from "../firebaseConfig";
 import { Ipost } from "../types/post-type";
 import Post from "./Post";
@@ -41,7 +48,38 @@ export default () => {
   // 접속할 때마다, Timeline이 보여질 때 마다
   useEffect(() => {
     // 서버(Firebase)에서 최신 게시글들 받아오기
-    fetchPosts();
+    // fetchPosts();
+
+    // realtime 으로 서버에서 최신 게시글 갱신
+    const fetchPostsRealtime = async () => {
+      // 1. Listener 활용해 실시간 상태 구독
+      let unsubscribe: Unsubscribe | null = null;
+      // 2. SERVER DB에서 최신 게시글 가져올 Query
+      const path = collection(firestore, "posts");
+      const condition = orderBy("createdAt", "desc");
+      const postsQuery = query(path, condition);
+      // 5. 최신 게시글 상태를 Listener에 구독(연결/연동)
+      unsubscribe = await onSnapshot(postsQuery, (snapshot) => {
+        // 4-1. 최신 게시글 정보
+        const timelinePosts = snapshot.docs.map((doc) => {
+          // 3. Query를 통해 받아온 게시글 정보 가공
+          const { createdAt, nickname, post, userId } = doc.data() as Ipost;
+          return {
+            createdAt,
+            nickname,
+            post,
+            userId,
+            id: doc.id,
+          };
+        });
+        // 4-2 최신 게시글 state에 저장
+        setPosts(timelinePosts);
+      });
+
+      // 6. Timeline 페이지 벗어날 시, 구독 종료
+    };
+
+    fetchPostsRealtime();
   }, []);
 
   // Page Design
